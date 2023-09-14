92da580c2b1841b24254256780e760af5f8362b7
cirleioliveira <shirley_1979_5@hotmail.com>
2023-09-14 19:34:46 -0400

    condicao ternaria

---
+++ Introdução e Estrutura Condicionais com Java/Condicoes Simples/condicoesSimples/src/resultadoEscolarCondTernaria.java
@@ -0,0 +1,10 @@
+public class resultadoEscolarCondTernaria {
+    public static void main(String[] args) {
+        int nota = 6;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        String resultados = nota >= 7 ? "Aprovado" : nota >=5 && nota < 7 ? "Recuperacao" : "Reprovado";
+        System.out.println(resultado);
+        System.out.println(resultados);
+    }
+    
+}
