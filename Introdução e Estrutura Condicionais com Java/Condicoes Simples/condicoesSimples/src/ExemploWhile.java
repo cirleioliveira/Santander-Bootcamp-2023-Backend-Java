46491a2a259be1b4c6e72bbabd58786c890bd473
cirleioliveira <shirley_1979_5@hotmail.com>
2023-09-15 12:36:46 -0400

    Estrutura de repetição While

---
+++ Introdução e Estrutura Condicionais com Java/Condicoes Simples/condicoesSimples/src/ExemploWhile.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+        public static void main(String[] args) {
+        double mesada = 50;
+
+        while (mesada > 0) {
+            Double valorDoce = valorAleatorio();
+            if(valorDoce > mesada)
+                valorDoce= mesada;
+
+            System.out.println("Doce do valor: " + valorDoce + " adicionado no carrinho: ");
+            mesada = mesada - valorDoce;
+        }
+        System.out.println("mesada: " + mesada);
+        System.out.println("Joaozinho gastou toda a sua mesada em doces");
+    }
+    private static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(2,20);
+    }
+    
+}
