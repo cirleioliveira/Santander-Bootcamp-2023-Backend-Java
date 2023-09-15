4f2e543028caed2fbed5dd31ab8e4af37663222d
cirleioliveira <shirley_1979_5@hotmail.com>
2023-09-14 21:05:27 -0400

    Estrutura de repeticao for

---
+++ Introdução e Estrutura Condicionais com Java/Condicoes Simples/condicoesSimples/src/exemploFor.java
@@ -0,0 +1,8 @@
+public class exemploFor {
+    public static void main(String[] args) {
+        for (int i = 0; i <=20; i++){
+            System.out.println("Contador de carneirinhos - " + i);
+        }
+    }
+    
+}
