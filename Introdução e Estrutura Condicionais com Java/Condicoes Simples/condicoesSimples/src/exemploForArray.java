99deb9667c163f4641088ed8eb8ccc7471bb1c59
cirleioliveira <shirley_1979_5@hotmail.com>
2023-09-14 21:20:20 -0400

    alteração1

---
+++ Introdução e Estrutura Condicionais com Java/Condicoes Simples/condicoesSimples/src/exemploForArray.java
@@ -0,0 +1,11 @@
+public class exemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"Felipe", "Jonas", "Julis", "Marcos" };
+
+        for (int i=0; i<alunos.length; i++){
+            System.out.println("O aluno no indice i = " + i + " é " + alunos[i]);
+        }
+
+    }
+    
+}
