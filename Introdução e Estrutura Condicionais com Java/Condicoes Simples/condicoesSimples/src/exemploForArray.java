f5381887d352f8f83607d84f706e4cbd6989d51a
cirleioliveira <shirley_1979_5@hotmail.com>
2023-09-14 21:28:59 -0400

    for em arrays alteracao

--- Introdução e Estrutura Condicionais com Java/Condicoes Simples/condicoesSimples/src/exemploForArray.java
+++ Introdução e Estrutura Condicionais com Java/Condicoes Simples/condicoesSimples/src/exemploForArray.java
@@ -1,11 +1,15 @@
 public class exemploForArray {
     public static void main(String[] args) {
-        String alunos[] = {"Felipe", "Jonas", "Julis", "Marcos" };
+        String alunos [] = {"Felipe", "Jonas", "Julis", "Marcos" };
 
-        for (int i=0; i<alunos.length; i++){
-            System.out.println("O aluno no indice i = " + i + " é " + alunos[i]);
+        for (String aluno : alunos){
+            System.out.println("O nome do aluno é: " + aluno); 
         }
-
+        
+        System.out.println("\n");
+       for (int i=0; i<alunos.length; i++){
+            System.out.println("O aluno no indice i = " + i + " é " + alunos[i]); 
+       }
     }
     
 }
